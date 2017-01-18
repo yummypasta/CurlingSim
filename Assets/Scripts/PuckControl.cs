492edd2308de62365750d12e990947a1ceafb223
Eric Yooniverse <emjyoon@gmail.com>
2017-01-17 17:28:36 -0800

    added velocity stuff, velocity counter

--- Assets/Scripts/PuckControl.cs
+++ Assets/Scripts/PuckControl.cs
@@ -8,10 +8,16 @@ public class PuckControl : MonoBehaviour
         toss,
         glide
     }
+    public Vector2 initForce;
+    public float threshold;
+    public float dragIncrease;
     state puckState;
+    Rigidbody2D rigid;
+
     // Use this for initialization
     void Start()
     {
+        rigid = GetComponent<Rigidbody2D>();
         puckState = state.toss;
     }
 
@@ -20,10 +26,27 @@ public class PuckControl : MonoBehaviour
     {
         if (puckState == state.toss)
         {
-            if(Input.touchCount > 0)
+            if (Input.GetKeyDown(KeyCode.Space) || Input.touchCount > 0)
             {
-
+                Debug.Log("Whee! Init glide!");
+                rigid.AddForce(initForce);
+                puckState = state.glide;
+                
             }
+        }
+        else if(puckState == state.glide)
+        {
+            //rigid.AddForce(new Vector2(0, (Input.GetTouch(0).deltaPosition.x + Input.GetTouch(0).deltaPosition.y) * 12));
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                Debug.Log("addforce");
+                rigid.AddForce(new Vector2(0, 10));
+            }
+            if(rigid.velocity.y < threshold)
+            {
+                Debug.Log("u ded get shrekt");
+            }
+            rigid.drag += dragIncrease;
         }
     }
 }
---
+++ Assets/Scripts/VelocityChecker.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class VelocityChecker : MonoBehaviour {
+    Text txt;
+    public Rigidbody2D body;
+	// Use this for initialization
+	void Start () {
+        txt = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        txt.text = body.velocity.y.ToString();
+	}
+}

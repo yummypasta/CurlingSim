b62c29f13589566221b610a53564bdf816b376ab
Eric Yooniverse <emjyoon@gmail.com>
2017-01-17 16:46:20 -0800

    added eVERYTHINGGGG

---
+++ Assets/Scripts/PuckControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuckControl : MonoBehaviour
+{
+    public enum state
+    {
+        toss,
+        glide
+    }
+    state puckState;
+    // Use this for initialization
+    void Start()
+    {
+        puckState = state.toss;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (puckState == state.toss)
+        {
+            if(Input.touchCount > 0)
+            {
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SetPos.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetPos : MonoBehaviour {
+    public Transform other;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(other.position.x, other.position.y, transform.position.z);
+	}
+}
